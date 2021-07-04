092b29975bc3902a347b52df2250410c122ff9b5
mkyj100 <mkyj100@gmail.com>
2021-07-04 19:16:53 +0900

    debug用ソース

--- Q1/main.rb
+++ Q1/main.rb
@@ -4,7 +4,7 @@ ultra = []
 fight_set = []
 set.to_i.times do |i|
     puts "必殺技回数を入力してください。"
-    ultra[i] = gets.chomp
+    ultra[i] = gets.chomp.to_i
     puts "敵のグループを入力してください。"
     fight_set[i] = gets.chomp.split
 end
@@ -13,21 +13,31 @@ index = 0
 result = []
 kill = 0
 fight_set.each do |enemy_grp|
-    enemy_grp.each do |enemy,i|
+    enemy_grp.each_with_index do |enemy,i|
+        enemy = enemy.to_i
+        puts "enemy :" + enemy.to_s
+        puts "i :" + i.to_s
         if enemy > 5
             if ultra[index] == 0
                 if i < enemy_grp.length-1
                     puts "False " + kill.to_s
+                    kill=0
+                    index+=1
                     break;
                 end
             else 
+                puts "ultra[index]:"+ ultra[index].to_s
+                puts ultra[index].class
                 ultra[index]-=1
                 kill+=enemy
             end
         else
             kill+=enemy
         end
+        if i == enemy_grp.length-1
+            puts "True "+ kill.to_s
+            kill=0
+        end
     end
-    puts "True "+ kill.to_s
     index+=1
 end
